dd85c1c5b163e304aad2ce232355fa72a87cfe1e
Konrad Strachan <konrad.strachan@gmail.com>
2026-02-27 14:31:17 +0000

    now installs from requirements.txt

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# Set up Python virtual environment
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install -r requirements.txt
+
+
 python3 src/main.py --market-events data/canned/market_events.csv --simulated-events data/canned/simulated_events.csv --out results.csv
